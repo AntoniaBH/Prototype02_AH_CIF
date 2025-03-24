402452c20e22eb251f198579f49cd41931f2743e
AntoniaBH <antoniaberta.hoch@ue-germany.de>
2025-03-24 12:52:06 +0100

    adjustments + fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,12 +13,10 @@ public class MainMenu : MonoBehaviour
     public Image transitionImage;
 
     //Quit Window Elements
-    public GameObject quitWindow;
     public RectTransform quitPolaroid;
     public RectTransform quitOptionsWindow;
 
     //Start Window Elements
-    public GameObject startWindow;
     public RectTransform startPolaroid;
     public RectTransform startOptionsWindow;
 
@@ -40,15 +38,7 @@ public class MainMenu : MonoBehaviour
         StartCoroutine(QuitGame());
     }
 
-    public void StartQuitTransition()
-    {
-        StartCoroutine(QuitWindowTransition());
-    }
-
-    public void StartStartTransition()
-    {
-        StartCoroutine(StartWindowTransition());
-    }
+   
 
     private IEnumerator PlayVideoAndStartGame()
     {
@@ -99,36 +89,5 @@ public class MainMenu : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
     }
 
-    private IEnumerator QuitWindowTransition()
-    {
-        quitWindow.SetActive(true);
-
-        yield return StartCoroutine(SlideIn(quitPolaroid, new Vector2(168, -8), new Vector2(Screen.width + 600f, -8), 2f));
-
-        yield return StartCoroutine(SlideIn(quitOptionsWindow, new Vector2(-567, 11), new Vector2(-905f, 11), 1f));
-    }
-
-    private IEnumerator StartWindowTransition()
-    {
-        startWindow.SetActive(true);
-
-        yield return StartCoroutine(SlideIn(startPolaroid, new Vector2(-175, -8), new Vector2(-200f, -8), 1f));
-
-        yield return StartCoroutine(SlideIn(startOptionsWindow, new Vector2(-5, 0), new Vector2(Screen.width + 200f, 0), 1f));
-    }
-
-    private IEnumerator SlideIn(RectTransform rectTransform, Vector2 targetPosition, Vector2 startPosition, float duration)
-    {
-        rectTransform.anchoredPosition = startPosition;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            rectTransform.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, elapsedTime / duration);
-            yield return null;
-        }
-
-        rectTransform.anchoredPosition = targetPosition;
-    }
+    
 }
