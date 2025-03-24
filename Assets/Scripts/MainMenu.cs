3cd3a4970af4ba7188d1dc55de2c5f81135c51c4
AntoniaBH <antoniaberta.hoch@ue-germany.de>
2025-03-24 10:06:01 +0100

    update 3

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
+    public RawImage videoRawImage;
+    public Image transitionImage;
+
+    private void Start()
+    {
+        Color color = transitionImage.color;
+        color.a = 0f;
+        transitionImage.color = color;
+        transitionImage.raycastTarget = false;
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(PlayVideoAndStartGame());
     }
 
     public void QuitGame()
@@ -16,4 +30,43 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    private IEnumerator PlayVideoAndStartGame()
+    {
+        videoRawImage.gameObject.SetActive(true);
+        videoPlayer.Play();
+
+        yield return new WaitWhile(() => videoPlayer.isPlaying);
+
+        yield return StartCoroutine(FadeToBlack(1.5f));
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+    }
+
+    private IEnumerator FadeToBlack(float duration)
+    {
+        transitionImage.gameObject.SetActive(true);
+        transitionImage.raycastTarget = true;
+        Color color = transitionImage.color;
+        color.a = 0f;
+        transitionImage.color = color;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / duration);
+            transitionImage.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        transitionImage.color = color;
+    }
 }
