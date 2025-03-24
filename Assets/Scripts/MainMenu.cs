8f2bd5153de88e6f5a243632c49e5bb7c5271179
AntoniaBH <antoniaberta.hoch@ue-germany.de>
2025-03-24 11:34:26 +0100

    update 6

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,6 +11,16 @@ public class MainMenu : MonoBehaviour
     public VideoPlayer videoPlayer;
     public RawImage videoRawImage;
     public Image transitionImage;
+
+    //Quit Window Elements
+    public GameObject quitWindow;
+    public RectTransform quitPolaroid;
+    public RectTransform quitOptionsWindow;
+
+    //Start Window Elements
+    public GameObject startWindow;
+    public RectTransform startPolaroid;
+    public RectTransform startOptionsWindow;
 
     private void Start()
     {
@@ -24,10 +35,19 @@ public class MainMenu : MonoBehaviour
         StartCoroutine(PlayVideoAndStartGame());
     }
 
-    public void QuitGame()
+    public void ButtonQuitGame()
     {
-        Debug.Log("Quit");
-        Application.Quit();
+        StartCoroutine(QuitGame());
+    }
+
+    public void StartQuitTransition()
+    {
+        StartCoroutine(QuitWindowTransition());
+    }
+
+    public void StartStartTransition()
+    {
+        StartCoroutine(StartWindowTransition());
     }
 
     private IEnumerator PlayVideoAndStartGame()
@@ -68,5 +88,47 @@ public class MainMenu : MonoBehaviour
 
         color.a = 1f;
         transitionImage.color = color;
+    }
+
+    private IEnumerator QuitGame()
+    {
+        yield return StartCoroutine(FadeToBlack(1f));
+
+        Debug.Log("Quit");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    private IEnumerator QuitWindowTransition()
+    {
+        quitWindow.SetActive(true);
+
+        yield return StartCoroutine(SlideIn(quitPolaroid, new Vector2(168, -8), new Vector2(Screen.width + 600f, -8), 2f));
+
+        yield return StartCoroutine(SlideIn(quitOptionsWindow, new Vector2(-567, 11), new Vector2(-905f, 11), 1f));
+    }
+
+    private IEnumerator StartWindowTransition()
+    {
+        startWindow.SetActive(true);
+
+        yield return StartCoroutine(SlideIn(startPolaroid, new Vector2(-175, -8), new Vector2(-200f, -8), 1f));
+
+        yield return StartCoroutine(SlideIn(startOptionsWindow, new Vector2(-5, 0), new Vector2(Screen.width + 200f, 0), 1f));
+    }
+
+    private IEnumerator SlideIn(RectTransform rectTransform, Vector2 targetPosition, Vector2 startPosition, float duration)
+    {
+        rectTransform.anchoredPosition = startPosition;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            rectTransform.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, elapsedTime / duration);
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = targetPosition;
     }
 }
